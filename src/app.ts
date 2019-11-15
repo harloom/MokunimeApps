303aec472fa904e6949bbd074b5d37652ef87e09
Harloom <harloom.developer@gmail.com>
2019-11-15 16:03:14 +0700

    1.settinng server

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import express,{ Application,Request,Response,NextFunction } from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 const app : Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+dotenv.config({
+  path: '.env'
+});	
 
 app.get('/',(req:Request,res : Response ) => {
 
     res.send('TS App is Running')
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 30000;
 
 
 app.listen(PORT,() => {
