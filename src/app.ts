7c42f6b9d68587ae0d3d4dfc6e868a394e658ca7
Harloom <harloom.developer@gmail.com>
2019-11-16 19:01:15 +0700

    1.test

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,27 @@ import express,{ Application,Request,Response,NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import socket from 'socket.io';
+import * as path from "path";
 
 const app : Application = express();
+// app.set("port", process.env.PORT || 3000);
+const http = require('http').Server(app);
+const io = socket(http)
+
+
+
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 dotenv.config({
   path: '.env'
 });	
-
+io.on("connection", function(socket: any) {
+  console.log("a user connected");
+});
 app.get('/',(req:Request,res : Response ) => {
-    res.send('TS App is Running')
+  res.sendFile(path.resolve("./src/index.html"));
 })
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,8 @@ dotenv.config({
   path: '.env'
 });	
 
-const db = 'mongodb://mongo:27017/test';
-Connect({ db });
+// const db = 'mongodb://mongo:27017/test';
+// Connect({ db });
 
 
 const PORT = process.env.PORT || 3000;
