d4d0162b96d9423c48a03d2a25e6fb460c8e541e
Harloom <harloom.developer@gmail.com>
2019-11-19 22:16:09 +0700

    1.berhasil

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,16 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import socket from 'socket.io';
 import * as path from "path";
+import  http from "http"
+
+
 
 const app : Application = express();
+const sokcetApp : Application = express();
+
 app.set("port", process.env.PORT || 3000);
-const http = require('http').Server(app);
-const io = socket(http)
+const server = new http.Server(app);
+const io = socket(server)
 
 
 app.use(bodyParser.json());
@@ -17,6 +22,12 @@ dotenv.config({
 });	
 io.on("connection", function(socket: any) {
   console.log("a user connected " + `${socket}`);
+    // whenever we receive a 'message' we log it out
+    socket.on("message", function(message: any) {
+      console.log(message);
+      socket.emit("messageBalas" ,"OK")
+      io.emit("broadcast", "ini Broadcast")
+    });
 });
 
 
@@ -25,4 +36,4 @@ io.on("connection", function(socket: any) {
 app.get('/',(req:Request,res : Response ) => {
   res.sendFile(path.resolve("./src/index.html"));
 })
-export default http;
+export default server;
